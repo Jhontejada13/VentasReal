9e34478cb002a29c353e4eb80e746a3b3cc68105
Jhon Tejada <jhontejada13@gmail.com>
2020-11-17 23:34:06 -0500

    Actualización proyecto, funcionalidades get, post,put,delete

--- WSVenta/WSVenta/Controllers/ClienteController.cs
+++ WSVenta/WSVenta/Controllers/ClienteController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WSVenta.Models;
+using WSVenta.Models.Response;
+using WSVenta.Models.ViewModels;
 
 namespace WSVenta.Controllers
 {
@@ -15,12 +17,97 @@ namespace WSVenta.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            using (VentaRealContext db = new VentaRealContext())
+            Respuesta oRespuesta = new Respuesta();
+            oRespuesta.Exito = 0;
+            try
             {
-                var list = db.Cliente.ToList();
-                return Ok(list);
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    var list = db.Cliente.OrderByDescending(d => d.Id).ToList();
+                    oRespuesta.Exito = 1;
+                    oRespuesta.Data = list;
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+            return Ok(oRespuesta);
+
+        }
+
+        [HttpPost]
+        public IActionResult Add(ClienteRequest oModel)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            oRespuesta.Exito = 0;
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    Cliente oCliente = new Cliente();
+                    oCliente.Nombre = oModel.Nombre;
+                    db.Cliente.Add(oCliente);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
             }
 
+            return Ok(oRespuesta);
+        }
+
+        [HttpPut]
+        public IActionResult Update(ClienteRequest oModel)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            oRespuesta.Exito = 0;
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    Cliente oCliente = db.Cliente.Find(oModel.id);
+                    oCliente.Nombre = oModel.Nombre;
+                    db.Entry(oCliente).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Respuesta oRespuesta = new Respuesta();
+            oRespuesta.Exito = 0;
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    Cliente oCliente = db.Cliente.Find(id);
+                    db.Remove(oCliente);
+                    db.SaveChanges();
+                    oRespuesta.Exito = 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+            }
+
+            return Ok(oRespuesta);
         }
     }
 }
--- WSVenta/WSVenta/Startup.cs
+++ WSVenta/WSVenta/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -15,6 +16,7 @@ namespace WSVenta
 {
     public class Startup
     {
+        readonly string MICors = "MiCors";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +27,16 @@ namespace WSVenta
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MICors, 
+                                builder => 
+                                {
+                                    builder.WithHeaders("*");
+                                    builder.WithOrigins("*");
+                                    builder.WithMethods("*");
+                                });
+            });
             services.AddControllers();
         }
 
@@ -39,6 +51,8 @@ namespace WSVenta
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(MICors);
 
             app.UseAuthorization();
 
